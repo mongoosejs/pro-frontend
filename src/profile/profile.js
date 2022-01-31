38977e075a9931604677ae9ad0f5aa2b66c14b77
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-01-31 13:29:19 -0500

    added desired functionality to the profile page of pro-frontend

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -3,18 +3,46 @@
 const appendCSS = require('../appendCSS');
 const css = require('./profile.css');
 const template = require('./profile.html').default;
+const axios = require('axios');
+const validator = require('validator');
 
 // appendCSS(css);
 
 module.exports = app => app.component('profile', {
   inject: ['auth'],
+  data: function() {
+    return {
+      company: '',
+      description: '',
+      logo: '',
+      name: '',
+      error: ''
+    }
+  },
   template: template,
   mounted: async function() {
-    await fetch('http://localhost:7071/api/updateSub');
+    console.log('this.auth', this.auth)
+    this.company = this.auth.user.companyName;
+    this.description = this.auth.user.description;
+    this.logo = this.auth.user.logo;
+    this.name = this.auth.user.githubUsername;
   },
   methods: {
     async updateSub() {
-
+      if(!validator.isURL(this.logo)) {
+        this.error = 'Please enter a valid url';
+        return;
+      } else {
+        this.error = '';
+      }
+      const res = await axios.post(`http://localhost:7071/api/updateSub`, 
+      { user: this.auth.user._id, company: this.company, description: this.description, logo: this.logo},
+      {headers: { authorization: this.auth.accessToken, 'Content-Type': 'application/json' } });
+      console.log('the response', res.data)
+      this.company = res.data.companyName;
+      this.description = res.data.description;
+      this.logo = res.data.logo;
+      this.name = res.data.githubUsername;
     }
   }
 });
